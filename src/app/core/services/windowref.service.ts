63afb9b6d5886dd70048d3d890e2b4c177d39dc4
Saavedra Sebastian <ssaavedraa4@upao.edu.pe>
2025-05-19 22:36:03 -0500

    Arreglos para despliegue

--- src/app/core/services/windowref.service.ts
+++ src/app/core/services/windowref.service.ts
@@ -1,10 +1,15 @@
+// window-ref.service.ts
 import { Injectable } from '@angular/core';
+
+function _window(): any {
+  return typeof window !== 'undefined' ? window : null;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class WindowRefService {
-  get nativeWindow(): Window | null {
-    return typeof window !== 'undefined' ? window : null;
+  get nativeWindow(): any {
+    return _window();
   }
 }
